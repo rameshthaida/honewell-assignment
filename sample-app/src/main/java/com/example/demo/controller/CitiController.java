7f1700ff76071e4c78e6bb979ac282d07cd5413e
Ramesh Thaida <ramesh.thaida@gmail.com>
2023-11-10 12:04:38 +0530

    initial commit

---
+++ sample-app/src/main/java/com/example/demo/controller/CitiController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.CitiDetailsDTO;
+import com.example.demo.service.CitiService;
+
+@RestController
+public class CitiController {
+
+	@Autowired
+	private CitiService citiService;
+
+	@GetMapping("/getCitiDetails")
+	public ResponseEntity<List<CitiDetailsDTO>> getCitiList() throws Exception {
+		return new ResponseEntity<List<CitiDetailsDTO>>(citiService.getCItiList(), HttpStatus.OK);
+	}
+
+}
---
+++ sample-app/src/main/java/com/example/demo/dto/CitiDetailsDTO.java
@@ -0,0 +1,41 @@
+package com.example.demo.dto;
+
+public class CitiDetailsDTO {
+
+	private String name;
+	private String state;
+	private String lat;
+	private String lon;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getLon() {
+		return lon;
+	}
+	public void setLon(String lon) {
+		this.lon = lon;
+	}
+	
+	
+	
+	
+	
+	
+}
+
+
